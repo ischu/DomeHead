c486c4524db49bb2b1cb5da4702e780b7c67a051
ianschu <ianschu7@gmail.com>
2019-03-11 20:59:00 -0400

    onsubmits added to form

---
+++ public/js/validate.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+$(".materialize-textarea").characterCounter();
+
+function validate() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function validateTitleForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function validateStoryForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function validateAuthorForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
+console.log("validate");
