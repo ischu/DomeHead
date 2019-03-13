7043341826d1eea859fa200d37bbd0d005871ec9
ianschu <ianschu7@gmail.com>
2019-03-12 20:20:55 -0400

    added password to submitAuthor function

--- models/author.js
+++ models/author.js
@@ -4,7 +4,10 @@ module.exports = function(sequelize, DataTypes) {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
+      unique: {
+        args: true,
+        msg: "Sorry, this author name is taken"
+      },
       validate: {
         // author names must be between 4 and
         len: [3, 20],
