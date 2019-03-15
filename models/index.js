3bd917e444f455a454bcc7001b4fe61de65bbefe
ianschu <ianschu7@gmail.com>
2019-03-15 01:02:15 -0400

    changed NODE_ENV

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
+var env = process.env.production || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
@@ -15,7 +15,7 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    config
+    config, 
   );
 }
 
